452cc6c60d9c897df4f9dfbade71777f390eba15
sbpawar122 <sbpawar7007@gmail.com>
2022-05-19 17:56:57 +0530

    added static method

---
+++ Staticmethod.java
@@ -0,0 +1,18 @@
+
+public class Staticmethod {
+	static int a =10;
+	public static void display()
+	
+	{
+		int a;
+		int b;
+		System.out.println("this is static dispaly method");
+		System.out.println("A  "+ this.a);
+	}
+	static void disp()
+	{
+		System.out.println("disp method print");
+	}
+	
+
+}
