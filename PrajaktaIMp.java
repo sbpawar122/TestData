648ce9992338d554068c4ade746cd9948a3fecf6
sbpawar122 <sbpawar7007@gmail.com>
2022-05-21 08:00:48 +0530

    added interface prajakta

---
+++ PrajaktaIMp.java
@@ -0,0 +1,19 @@
+package Abstract;
+
+import Interface.Prajaktaimpiment;
+
+public class PrajaktaIMp {
+
+	public static void main(String[] args) {
+		Prajaktaimpiment a= new Prajaktaimpiment();
+		a.angry();
+		a.busy();
+		a.hungry();
+		a.fullname();
+		a.display();
+		
+		
+
+	}
+
+}
